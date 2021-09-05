55e3bdc4b9e1cb8087b4f2362ddc31ad9171fbc0
Berrybunny015 <berrybunny015@gmail.com>
2021-09-05 15:58:11 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        //variable declaration
+        double conversionFactor = 0.09290304;
+
+        System.out.println("\nWhat is the length of the room in feet? "); //output #1
+        Scanner lengthInput = new Scanner(System.in); //scan for length of room in feet
+        int length = lengthInput.nextInt();
+
+        System.out.println("\nWhat is the width of the room in feet? "); //output #2
+        Scanner widthInput = new Scanner(System.in); //scan for width of room in feet
+        int width = widthInput.nextInt();
+
+        System.out.println("\nYou entered dimensions of " + length + " feet by " + width + " feet."); //output #3
+
+        int areaSquareFeet = length * width; //find the area in square feet
+        double areaSquareMeters = areaSquareFeet * conversionFactor; //find the area in square meters
+        DecimalFormat decimalFormat = new DecimalFormat("###.###"); //round up the area in square meters to max 3 decimals
+
+        System.out.println("The area is \n" + areaSquareFeet + " square feet \n" + decimalFormat.format(areaSquareMeters) + " square meters"); //output #4
+
+    }
+}
